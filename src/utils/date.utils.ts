00ee70b1b6dfbd065c9b488626094c16a49f81c3
stresscontroller <talentdev.dj@gmail.com>
2026-03-18 22:11:41 +0900

    init

--- src/utils/date.utils.ts
+++
@@ -1,5 +0,0 @@
-// utils/dateUtils.ts
-export function addHours(date: Date, hours: number): Date {
-    return new Date(date.getTime() + hours * 60 * 60 * 1000)
-}
-  
--- src/validations/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as envValidation } from './env.validation';
